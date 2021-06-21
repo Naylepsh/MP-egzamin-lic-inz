02b242bdcf0135bf25409f9328545d51c1b8d688
Naylepsh <naylepsh@gmail.com>
2021-06-21 21:23:38 +0200

    cleanup

--- 2020-luty/02b.rkt
+++ 2020-luty/02b.rkt
@@ -7,11 +7,10 @@
 
 (define (sort-bits xs)
   (define (create-sorted-bit-list zeros ones)
-    (if (> zeros 0)
-      (cons 0 (create-sorted-bit-list (- zeros 1) ones))
-      (if (> ones 0)
-        (cons 1 (create-sorted-bit-list zeros (- ones 1)))
-        '())))
+    (cond 
+      [(> zeros 0) (cons 0 (create-sorted-bit-list (- zeros 1) ones ))]
+      [(> ones 0) (cons 1 (create-sorted-bit-list zeros (- ones 1)))]
+      [else null]))
   (define zeros (num-of-occurrences 0 xs))
   (define ones (num-of-occurrences 1 xs))
   (create-sorted-bit-list zeros ones))
